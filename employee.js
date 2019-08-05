18ece0a9193869b0d4cf0aa0c628ff8f04d1e7de
Bryan Pato <bryanpato87@gmail.com>
2019-08-05 17:27:11 +0100

    fichaCol

---
+++ employee.js
@@ -0,0 +1,212 @@
+class Employee {
+    constructor(name, address, location, postalCode, email, phoneNumber, academicQualifications, academicInstitution, course,
+        cc, ccValidity, country, birthDate, nif, niss, maritalStatus, maritalWorkStatus, dependents, dependentsAges, iban, swift,
+        professionalCategory, specialTech, otherTech, admissionDate, grossSalary, contractType, extras, twelfths, twelfths5050,
+        twelfths112, foodAllowence, workSchedule, sports, sportsType, otherActivities, suggestedActivities, hobbies, otherInterests,
+        client, clientSector, project, fare, healthInsurance, companyFinancing, extensible, insuranceRelativeName, insuranceRelativeBirthDate,
+        companyFinancingRelative, commentarySection) {
+
+            this.name=name;
+            this.address=address;
+            this.location=location;
+            this.postalCode=postalCode;
+            this.email=email;
+            this.phoneNumber=phoneNumber;
+            this.academicQualifications=academicQualifications;
+            this.academicInstitution=academicInstitution;
+            this.course=course;
+            this.cc=cc;
+            this.ccValidity=ccValidity;
+            this.country=country;
+            this.birthDate=birthDate;
+            this.nif=nif;
+            this.niss=niss;
+            this.maritalStatus=maritalStatus;
+            this.maritalWorkStatus=maritalWorkStatus;
+            this.dependents=dependents;
+            this.dependentsAges=dependentsAges;
+            this.iban=iban;
+            this.swift=swift;
+            this.professionalCategory=professionalCategory;
+            this.specialTech=specialTech;
+            this.otherTech=otherTech;
+            this.admissionDate=admissionDate;
+            this.grossSalary=grossSalary;
+            this.contractType=contractType;
+            this.extras=extras;
+            this.twelfths=twelfths;
+            this.twelfths5050=twelfths5050;
+            this.twelfths112=twelfths112;
+            this.foodAllowence=foodAllowence;
+            this.workSchedule=workSchedule;
+            this.sports=sports;
+            this.sportsType=sportsType;
+            this.otherActivities=otherActivities;
+            this.suggestedActivities=suggestedActivities;
+            this.hobbies=hobbies;
+            this.otherInterests=otherInterests;
+            this.client=client;
+            this.clientSector=clientSector;
+            this.project=project;
+            this.fare=fare;
+            this.healthInsurance=healthInsurance;
+            this.companyFinancing=companyFinancing;
+            this.extensible=extensible;
+            this.insuranceRelativeName=insuranceRelativeName;
+            this.insuranceRelativeBirthDate=insuranceRelativeBirthDate;
+            this.companyFinancingRelative=companyFinancingRelative;
+            this.commentarySection=commentarySection;
+         }
+         getName(){
+             return this.name;
+         }
+         getAddress(){
+            return this.address;
+        }
+        getLocation(){
+            return this.location;
+        }
+        getPostalCode(){
+            return this.postalCode;
+        }
+        getEmail(){
+            return this.email;
+        }
+        getPhoneNumber(){
+            return this.phoneNumber;
+        }
+        getAcademicQuallifications(){
+            return this.academicQualifications;
+        }
+        getAcademicInstitution(){
+            return this.academicInstitution;
+        }
+        getCourse(){
+            return this.course;
+        }
+        getCc(){
+            return this.cc;
+        }
+        getccValidity(){
+            return this.ccValidity;
+        }
+        getCountry(){
+            return this.country;
+        }
+        getBirthDate(){
+            return this.birthDate;
+        }
+        getNif(){
+            return this.nif;
+        }
+        getNiss(){
+            return this.niss;
+        }
+        getMaritalStatus(){
+            return this.maritalStatus;
+        }
+        getMaritalWorkStatus(){
+            return this.maritalWorkStatus;
+        }
+        getDependents(){
+            return this.dependents;
+        }
+        getDependentsAges(){
+            return this.dependentsAges;
+        }
+        getIban(){
+            return this.iban;
+        }
+        getSwift(){
+            return this.swift;
+        }
+        getProfessionalCategory(){
+            return this.professionalCategory;
+        }
+        getSpecialTech(){
+            return this.specialTech;
+        }
+        getOtherTech(){
+            return this.otherTech;
+        }
+        getAdmissionDate(){
+            return this.admissionDate;
+        }
+        getGrossSalary(){
+            return this.grossSalary;
+        }
+        getContractType(){
+            return this.contractType;
+        }
+        getExtras(){
+            return this.extras;
+        }
+        getTwelfths(){
+            return this.twelfths;
+        }
+        getTwelfths5050(){
+            return this.twelfths5050;
+        }
+        getTwelfths112(){
+            return this.twelfths112;
+        }
+        getFoodAllowence(){
+            return this.foodAllowence;
+        }
+        getWorkSchedule(){
+            return this.workSchedule;
+        }
+        getSports(){
+            return this.sports;
+        }
+        getSportsType(){
+            return this.sportsType;
+        }
+        getOtherActivities(){
+            return this.otherActivities;
+        }
+        getSuggestedActivities(){
+            return this.suggestedActivities;
+        }
+        getHobbies(){
+            return this.hobbies;
+        }
+        getOtherInterests(){
+            return this.otherInterests;
+        }
+        getClient(){
+            return this.client;
+        }
+        getClientSector(){
+            return this.clientSector;
+        }
+        getProject(){
+            return this.project;
+        }
+        getFare(){
+            return this.fare;
+        }
+        getHealthInsurance(){
+            return this.healthInsurance;
+        }
+        getCompanyFinancing(){
+            return this.companyFinancing;
+        }
+        getExtensible(){
+            return this.extensible;
+        }
+        getInsuranceRelativeName(){
+            return this.insuranceRelativeName;
+        }
+        getInsuranceRelativeBirthDate(){
+            return this.insuranceRelativeBirthDate;
+        }
+        getCompanyFinancingRelative(){
+            return this.companyFinancingRelative;
+        }
+        getCommentarySection(){
+            return this.commentarySection;
+        }
+        
+
+}
