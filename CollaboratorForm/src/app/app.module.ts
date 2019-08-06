04dffe86098d719389aaeff49e8a7e8fe226d069
FilipeLobo13 <52419204+FilipeLobo13@users.noreply.github.com>
2019-08-07 00:24:26 +0100

    importHttpModule

--- CollaboratorForm/src/app/app.module.ts
+++ CollaboratorForm/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component'
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { LoginComponent } from './login/login.component'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
